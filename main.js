d543e5389937f773cf53e7118b0930790e41f76e
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-02 15:49:40 +0100

    aggiunto voci menu con vue

---
+++ main.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+      menuOptions:[
+        'Home',
+        'Who We Are',
+        'What We Do',
+        'Where We Work',
+        'Careers',
+        'News'
+      ]
+
+
+
+    },
+
+});
