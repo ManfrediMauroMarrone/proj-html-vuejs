301dc52658984519c7ef877b8a6d83880922db3e
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-03 11:51:09 +0100

    aggiunto data e ciclo per section our news

--- main.js
+++ main.js
@@ -30,7 +30,26 @@ var app = new Vue({
         label:'Get In Touch Now',
         type: 'BUTTON'
       }
+    ],
+      news:[
+        {
+          label: 'Why You Need A SEO Agency Now',
+          date: 'November 1st, 2017',
+          background: 'images/related-service-2-700x441.jpg',
+          info: 'ALorem ipsum dolor sit amet, consectetur adipiscing elit. In et scelerisque sem. Nunc molestie neque augue, at gravida mi blandit'
+        },
+        {
+          label: 'SEO Tips For Your Startup',
+          date: 'November 1st, 2017',
+          background: 'images/related-service-4-700x441.jpg',
+          info: 'ALorem ipsum dolor sit amet, consectetur adipiscing elit. In et scelerisque sem. Nunc molestie neque augue, at gravida mi blandit'
+        },
+        {
+          label: 'Image Optimization For Your Site',
+          date: 'November 1st, 2017',
+          background: 'images/blog-post-6-700x441.jpg',
+          info: 'ALorem ipsum dolor sit amet, consectetur adipiscing elit. In et scelerisque sem. Nunc molestie neque augue, at gravida mi blandit'
+        }
       ]
     },
-
 });
