a4808c805f1fbdd40dfd250c74e067f889df1181
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-03 12:02:40 +0100

    aggiunto data e ciclo per section new-template

--- main.js
+++ main.js
@@ -31,25 +31,44 @@ var app = new Vue({
         type: 'BUTTON'
       }
     ],
-      news:[
-        {
-          label: 'Why You Need A SEO Agency Now',
-          date: 'November 1st, 2017',
-          background: 'images/related-service-2-700x441.jpg',
-          info: 'ALorem ipsum dolor sit amet, consectetur adipiscing elit. In et scelerisque sem. Nunc molestie neque augue, at gravida mi blandit'
-        },
-        {
-          label: 'SEO Tips For Your Startup',
-          date: 'November 1st, 2017',
-          background: 'images/related-service-4-700x441.jpg',
-          info: 'ALorem ipsum dolor sit amet, consectetur adipiscing elit. In et scelerisque sem. Nunc molestie neque augue, at gravida mi blandit'
-        },
-        {
-          label: 'Image Optimization For Your Site',
-          date: 'November 1st, 2017',
-          background: 'images/blog-post-6-700x441.jpg',
-          info: 'ALorem ipsum dolor sit amet, consectetur adipiscing elit. In et scelerisque sem. Nunc molestie neque augue, at gravida mi blandit'
-        }
-      ]
+
+    newTemplate: [
+      {
+        icon: 'fas fa-road',
+        label: 'Establish Goals',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      {
+        icon: 'fas fa-video',
+        label: 'Word With A Team',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      {
+        icon: 'fas fa-tint',
+        label: 'Get Results',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      }
+    ],
+
+    news:[
+      {
+        label: 'Why You Need A SEO Agency Now',
+        date: 'November 1st, 2017',
+        background: 'images/related-service-2-700x441.jpg',
+        info: 'ALorem ipsum dolor sit amet, consectetur adipiscing elit. In et scelerisque sem. Nunc molestie neque augue, at gravida mi blandit'
+      },
+      {
+        label: 'SEO Tips For Your Startup',
+        date: 'November 1st, 2017',
+        background: 'images/related-service-4-700x441.jpg',
+        info: 'ALorem ipsum dolor sit amet, consectetur adipiscing elit. In et scelerisque sem. Nunc molestie neque augue, at gravida mi blandit'
+      },
+      {
+        label: 'Image Optimization For Your Site',
+        date: 'November 1st, 2017',
+        background: 'images/blog-post-6-700x441.jpg',
+        info: 'ALorem ipsum dolor sit amet, consectetur adipiscing elit. In et scelerisque sem. Nunc molestie neque augue, at gravida mi blandit'
+      }
+    ]
     },
 });
